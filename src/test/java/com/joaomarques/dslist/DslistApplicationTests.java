1008a40c07bfd9e1897b7227f5d7299e54c5a18c
marques-joao <jvitormarques79@gmail.com>
2023-07-13 16:19:55 -0300

    project created

---
+++ src/test/java/com/joaomarques/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaomarques.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
